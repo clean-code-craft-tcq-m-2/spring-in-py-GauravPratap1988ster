28eaa74dc5af6d6beda21050e2d59571ba2ec2c3
GauravPratap1988ster <115794844+GauravPratap1988ster@users.noreply.github.com>
2022-10-22 21:24:15 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,8 +1,4 @@
 def calculateStats(numbers):
-   if len(numbers) == 0:
-      return float("nan")
-   return {"avg": sum(numbers) / len(numbers), "max": max(numbers), "min": min(numbers)}
-
-
-   #def __init__(self, numbers) -> None:
-   #  self.numbers = numbers
+    if len(numbers)==0:
+        return float("nan")
+    return {"avg": sum(numbers)/len(numbers), "max": max(numbers), "min": min(numbers)}
